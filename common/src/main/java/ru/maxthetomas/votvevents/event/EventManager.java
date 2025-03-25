7334ab720fb8477073a6ff257d4ad3ba4fb803d9
maxthetomas <max@maxthetomas.ru>
2025-03-25 21:06:06 +0400

    refactor eventmanager

--- common/src/main/java/ru/maxthetomas/votvevents/event/EventManager.java
+++ common/src/main/java/ru/maxthetomas/votvevents/event/EventManager.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import ru.maxthetomas.votvevents.behaviour.IBehaviour;
 import ru.maxthetomas.votvevents.util.ResourceUtil;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,45 +19,21 @@ import java.util.Set;
 public class EventManager extends SimplePreparableReloadListener<HashMap<ResourceLocation, EventResource>> {
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    private List<ActiveEvent> activeEvents = new ArrayList<>();
     private HashMap<ResourceLocation, EventResource> registeredEvents;
+    private List<ActiveEvent> activeEvents = new ArrayList<>();
 
-    @Override
-    protected @NotNull HashMap<ResourceLocation, EventResource> prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
-        var events = new HashMap<ResourceLocation, EventResource>();
-
-        resourceManager.listResources("events", (path) -> path.getPath().endsWith(".json")).forEach((loc, resource) -> {
-            var evt = ResourceUtil.getJsonResource(resourceManager, loc);
-
-            if (evt == null || !evt.isJsonObject()) {
-                LOGGER.warn("Failed to parse event resource at location {}", loc);
-                return;
-            }
-
-            var res = EventResource.buildEventResourceFromJson(evt.getAsJsonObject());
-
-            if (res == null) {
-                LOGGER.warn("Failed to parse event resource at location {}", loc);
-                return;
-            }
-
-            var ns = loc.getNamespace();
-            var p = loc.getPath().replace(".json", "").replace("events/", "");
-
-            events.put(ResourceLocation.fromNamespaceAndPath(ns, p), res);
-        });
-
-        return events;
-    }
-
-    @Override
-    protected void apply(HashMap<ResourceLocation, EventResource> object, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
-        this.registeredEvents = object;
-        LOGGER.info("Successfully reloaded events!");
-    }
+    // Getters
 
     public List<ActiveEvent> getActiveEvents() {
         return activeEvents;
+    }
+
+    public @Nullable EventResource getEvent(ResourceLocation location) {
+        return this.registeredEvents.getOrDefault(location, null);
+    }
+
+    public Set<ResourceLocation> getRegisteredEvents() {
+        return registeredEvents.keySet();
     }
 
     /**
@@ -102,11 +77,43 @@ public class EventManager extends SimplePreparableReloadListener<HashMap<Resourc
         return activeEvents.contains(event);
     }
 
-    public @Nullable EventResource getEvent(ResourceLocation location) {
-        return this.registeredEvents.getOrDefault(location, null);
+
+    /*
+     * Following methods are used to reload events from
+     * resources, using Minecraft's reload system.
+     */
+
+    @Override
+    protected @NotNull HashMap<ResourceLocation, EventResource> prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
+        var events = new HashMap<ResourceLocation, EventResource>();
+
+        resourceManager.listResources("events", (path) -> path.getPath().endsWith(".json")).forEach((loc, resource) -> {
+            var evt = ResourceUtil.getJsonResource(resourceManager, loc);
+
+            if (evt == null || !evt.isJsonObject()) {
+                LOGGER.warn("Failed to parse event resource at location {}", loc);
+                return;
+            }
+
+            var res = EventResource.buildEventResourceFromJson(evt.getAsJsonObject());
+
+            if (res == null) {
+                LOGGER.warn("Failed to parse event resource at location {}", loc);
+                return;
+            }
+
+            var ns = loc.getNamespace();
+            var p = loc.getPath().replace(".json", "").replace("events/", "");
+
+            events.put(ResourceLocation.fromNamespaceAndPath(ns, p), res);
+        });
+
+        return events;
     }
 
-    public Set<ResourceLocation> getRegisteredEvents() {
-        return registeredEvents.keySet();
+    @Override
+    protected void apply(HashMap<ResourceLocation, EventResource> object, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
+        this.registeredEvents = object;
+        LOGGER.info("Successfully reloaded events!");
     }
 }
