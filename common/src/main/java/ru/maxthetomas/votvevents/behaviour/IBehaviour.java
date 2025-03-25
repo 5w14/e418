73d5a670802dc81d1fae6010f1ec87143415becc
CaptainSqrBeard <iliya.11.levchenko@gmail.com>
2025-03-25 19:42:39 +0600

    Fix docs

--- common/src/main/java/ru/maxthetomas/votvevents/behaviour/IBehaviour.java
+++ common/src/main/java/ru/maxthetomas/votvevents/behaviour/IBehaviour.java
@@ -9,7 +9,7 @@ public interface IBehaviour {
     /**
      * Can this behaviour run.
      * Behaviours are generic. This should return false only if behaviour really can't run.
-     * If you want your event don't run in specific conditions (i.e. only at night), use {@link IBehaviour}
+     * If you want your event don't run in specific conditions (i.e. only at night), use {@link ru.maxthetomas.votvevents.condition.ICondition}
      * @return Is condition can run.
      */
     default boolean canRun(){
