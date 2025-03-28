3a1e4cb5d0194b03902e4157ae0471514ab30c95
CaptainSqrBeard <iliya.11.levchenko@gmail.com>
2025-03-29 04:57:50 +0600

    Added queued events feature

--- common/src/main/java/ru/maxthetomas/votvevents/debug/EventCommand.java
+++ common/src/main/java/ru/maxthetomas/votvevents/debug/EventCommand.java
@@ -1,6 +1,7 @@
 package ru.maxthetomas.votvevents.debug;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
@@ -28,30 +29,57 @@ public class EventCommand {
 
         dispatcher.register(LiteralArgumentBuilder.<CommandSourceStack>literal("event")
                 .requires(s -> s.hasPermission(2)) // "Game Master" (docs: https://minecraft.wiki/w/Permission_level)
-                .then(startSubcommand())
+                .then(startEvent())
+                .then(queueEvent())
                 .then(printEvents())
+                .then(printQueuedEvents())
         );
     }
 
 
     /**
-     * Creates a argument tree for the /event start command
+     * Creates an argument tree for the /event start command
      */
-    private static LiteralArgumentBuilder<CommandSourceStack> startSubcommand() {
+    private static LiteralArgumentBuilder<CommandSourceStack> startEvent() {
         return LiteralArgumentBuilder.<CommandSourceStack>literal("start")
                 .then(
                         RequiredArgumentBuilder.<CommandSourceStack, ResourceLocation>argument("event", ResourceLocationArgument.id())
-                                .executes(EventCommand::executeStartSubommand)
+                                .executes(EventCommand::executeStartSubcommand)
                                 .suggests(EventCommand::getEventSuggestions)
                                 .then(
                                         Commands.literal("force")
-                                                .executes(EventCommand::executeStartSubommand)
+                                                .executes(EventCommand::executeStartSubcommand)
                                 )
                 );
     }
 
     /**
-     * Prints out all loaded commands
+     * Creates an argument tree for the /event queue command
+     */
+    private static LiteralArgumentBuilder<CommandSourceStack> queueEvent() {
+        return LiteralArgumentBuilder.<CommandSourceStack>literal("queue")
+                .then(
+                        RequiredArgumentBuilder.<CommandSourceStack, ResourceLocation>argument("event", ResourceLocationArgument.id())
+                                .executes(EventCommand::executeQueueNoTimeoutSubcommand)
+                                .suggests(EventCommand::getEventSuggestions)
+                                .then(
+                                        RequiredArgumentBuilder.<CommandSourceStack, Integer>argument("timeout", IntegerArgumentType.integer(1))
+                                                .executes(EventCommand::executeQueueSubcommand)
+                                                .then(
+                                                        Commands.literal("force")
+                                                                .executes(EventCommand::executeQueueSubcommand)
+                                                )
+                                )
+                                .then(
+                                        Commands.literal("force")
+                                                .executes(EventCommand::executeQueueNoTimeoutSubcommand)
+                                )
+
+                );
+    }
+
+    /**
+     * Prints out all loaded events
      */
     private static LiteralArgumentBuilder<CommandSourceStack> printEvents() {
         return LiteralArgumentBuilder.<CommandSourceStack>literal("print")
@@ -59,9 +87,17 @@ public class EventCommand {
     }
 
     /**
+     * Prints out all queued events
+     */
+    private static LiteralArgumentBuilder<CommandSourceStack> printQueuedEvents() {
+        return LiteralArgumentBuilder.<CommandSourceStack>literal("print_queued")
+                .executes(EventCommand::executePrintQueuedEvents);
+    }
+
+    /**
      * Executes the /event start command with all possible arguments
      */
-    private static int executeStartSubommand(CommandContext<CommandSourceStack> context) {
+    private static int executeStartSubcommand(CommandContext<CommandSourceStack> context) {
         var eventLoc = ResourceLocationArgument.getId(context, "event");
         var isForced = context.getNodes().getLast().getNode().getName().equals("force");
 
@@ -100,6 +136,91 @@ public class EventCommand {
     }
 
     /**
+     * Executes the /event queue command with timeout
+     */
+    private static int executeQueueSubcommand(CommandContext<CommandSourceStack> context) {
+        var eventLoc = ResourceLocationArgument.getId(context, "event");
+        var timeout = IntegerArgumentType.getInteger(context, "timeout");
+        var isForced = context.getNodes().getLast().getNode().getName().equals("force");
+
+        var manager = VotvEvents.getEventManager();
+        var event = manager.getEvent(eventLoc);
+
+        if (event == null) {
+            context.getSource().sendFailure(
+                    Component.translatable("votvevents.commands.event.start.not_found", eventLoc.toString())
+                            .withStyle(ChatFormatting.RED)
+            );
+            return 0;
+        }
+
+        var eventContext = new EventContext(context.getSource().getServer())
+                .withPlayer(context.getSource().getPlayer())
+                .withForced(isForced);
+
+
+        var isQueued = VotvEvents.getEventManager().queueEvent(event, eventContext, timeout);
+
+        if (!isQueued) {
+            context.getSource().sendFailure(
+                    Component.translatable("votvevents.commands.event.queue.fail", formatEvent(event))
+                            .withStyle(ChatFormatting.RED)
+            );
+            return 0;
+        }
+
+        // Sends a success message, with the event name and description as hover text
+        context.getSource().sendSuccess(
+                () -> Component.translatable("votvevents.commands.event.queue.success" +
+                                (isForced ? ".force" : ""),
+                        formatEvent(event)), true);
+
+        return 1;
+    }
+
+    /**
+     * Executes the /event queue command without timeout
+     */
+    private static int executeQueueNoTimeoutSubcommand(CommandContext<CommandSourceStack> context) {
+        var eventLoc = ResourceLocationArgument.getId(context, "event");
+        var isForced = context.getNodes().getLast().getNode().getName().equals("force");
+
+        var manager = VotvEvents.getEventManager();
+        var event = manager.getEvent(eventLoc);
+
+        if (event == null) {
+            context.getSource().sendFailure(
+                    Component.translatable("votvevents.commands.event.start.not_found", eventLoc.toString())
+                            .withStyle(ChatFormatting.RED)
+            );
+            return 0;
+        }
+
+        var eventContext = new EventContext(context.getSource().getServer())
+                .withPlayer(context.getSource().getPlayer())
+                .withForced(isForced);
+
+
+        var isQueued = VotvEvents.getEventManager().queueEvent(event, eventContext);
+
+        if (!isQueued) {
+            context.getSource().sendFailure(
+                    Component.translatable("votvevents.commands.event.queue.fail", formatEvent(event))
+                            .withStyle(ChatFormatting.RED)
+            );
+            return 0;
+        }
+
+        // Sends a success message, with the event name and description as hover text
+        context.getSource().sendSuccess(
+                () -> Component.translatable("votvevents.commands.event.queue.success" +
+                                (isForced ? ".force" : ""),
+                        formatEvent(event)), true);
+
+        return 1;
+    }
+
+    /**
      * Executes the print command
      */
     private static int executePrintEvents(CommandContext<CommandSourceStack> context) {
@@ -108,6 +229,21 @@ public class EventCommand {
                         ComponentUtils.formatList(VotvEvents.getEventManager().getRegisteredEvents(),
                                 Component.literal("\n"),
                                 (e) -> Component.literal(" - " + e.toString()))
+                ), false);
+
+        return 1;
+    }
+
+    /**
+     * Executes the print queued events command
+     */
+    private static int executePrintQueuedEvents(CommandContext<CommandSourceStack> context) {
+        // TODO: make it also show timeout
+        context.getSource().sendSuccess(
+                () -> Component.translatable("votvevents.commands.event.print_queued",
+                        ComponentUtils.formatList(VotvEvents.getEventManager().getQueuedEvents(),
+                                Component.literal("\n"),
+                                (e) -> Component.literal(" - " + e.resource.name()))
                 ), false);
 
         return 1;
--- common/src/main/java/ru/maxthetomas/votvevents/event/EventManager.java
+++ common/src/main/java/ru/maxthetomas/votvevents/event/EventManager.java
@@ -4,8 +4,10 @@ import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 import com.mojang.logging.LogUtils;
 import com.mojang.serialization.JsonOps;
+import dev.architectury.event.events.common.TickEvent;
 import net.minecraft.resources.FileToIdConverter;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
@@ -13,6 +15,7 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
+import ru.maxthetomas.votvevents.VotvEvents;
 import ru.maxthetomas.votvevents.behaviour.IBehaviour;
 
 import java.io.BufferedReader;
@@ -23,6 +26,7 @@ import java.util.*;
 public class EventManager extends SimplePreparableReloadListener<Map<ResourceLocation, EventResource>> {
     private static final Logger LOGGER = LogUtils.getLogger();
     private final List<ActiveEvent> activeEvents = new ArrayList<>();
+    private final List<QueuedEvent> queuedEvents = new ArrayList<>();
     private Map<ResourceLocation, EventResource> registeredEvents;
 
     // Getters
@@ -30,12 +34,39 @@ public class EventManager extends SimplePreparableReloadListener<Map<ResourceLoc
         return activeEvents;
     }
 
+    public List<QueuedEvent> getQueuedEvents() {
+        return queuedEvents;
+    }
+
     public @Nullable EventResource getEvent(ResourceLocation location) {
         return this.registeredEvents.getOrDefault(location, null);
     }
 
+    public EventManager() {
+        TickEvent.SERVER_POST.register(EventManager::tick);
+    }
+
+    private static void tick(MinecraftServer server) {
+        VotvEvents.getEventManager().queuedEvents.removeIf((QueuedEvent queuedEvent) -> {
+            // Check timeout time
+            if (queuedEvent.timeoutTick != null && queuedEvent.timeoutTick < server.overworld().getGameTime()) {
+                LOGGER.info("Dequeued event {} (timed out).", queuedEvent.resource.name());
+                return true;
+            }
+
+            // Run event if it can
+            if (queuedEvent.resource.canRun(queuedEvent.context)) {
+                LOGGER.info("Running event {} from queue.", queuedEvent.resource.name());
+                VotvEvents.getEventManager().runEvent(queuedEvent.resource, queuedEvent.context);
+                return true;
+            }
+            return false;
+        });
+    }
+
     /**
-     * TODO: Make it only get non-debug events
+     * TODO: Implement event list with weights.
+     * Returns random registered event.
      *
      * @return random registered event
      */
@@ -77,6 +108,59 @@ public class EventManager extends SimplePreparableReloadListener<Map<ResourceLoc
         activeEvent.updateState();
 
         return activeEvent;
+    }
+
+    /**
+     * Adds event to queue. Queued events will run the moment their run conditions are met.
+     *
+     * @param resource Event to queue.
+     * @param context  Context of event.
+     * @param timeout  Time for event to be dequeued.
+     * @return Is event was queued or was run instantly.
+     */
+    public boolean queueEvent(EventResource resource, EventContext context, int timeout) {
+        var activeEvent = runEvent(resource, context);
+        if (activeEvent != null)
+            return true;
+
+        if (!resource.canQueue(context))
+            return false;
+
+        // TODO: something bad could happen with context if we would use it later. Should be fixed.
+        long timeoutTick;
+
+        timeoutTick = context.getServer().overworld().getGameTime() + timeout;
+
+        queuedEvents.add(new QueuedEvent(resource, context, timeoutTick));
+        LOGGER.info("Queued event {}.", resource.name());
+        return true;
+    }
+
+    /**
+     * Adds event to queue. Queued events will run the moment their run conditions are met.
+     *
+     * @param resource Event to queue.
+     * @param context  Context of event.
+     * @return Is event was queued or was run instantly.
+     */
+    public boolean queueEvent(EventResource resource, EventContext context) {
+        var activeEvent = runEvent(resource, context);
+        if (activeEvent != null)
+            return true;
+
+        if (!resource.canQueue(context))
+            return false;
+
+        // TODO: something bad could happen with context if we would use it later. Should be fixed.
+        queuedEvents.add(new QueuedEvent(resource, context, null));
+        LOGGER.info("Queued event {}.", resource.name());
+        return true;
+    }
+
+    public void dequeueEvent(QueuedEvent queuedEvent) {
+        LOGGER.info("Dequeued event {}", queuedEvent.resource.name());
+
+        queuedEvents.remove(queuedEvent);
     }
 
     /**
