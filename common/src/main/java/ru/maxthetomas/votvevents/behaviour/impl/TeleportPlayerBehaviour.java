9a2d96046e22eebb69910e11ee8047dc8ba4d074
iller_saver <illersaver@gmail.com>
2025-04-03 22:30:00 +0300

    TeleportBehaviour refactor and a new dimension

--- common/src/main/java/ru/maxthetomas/votvevents/behaviour/impl/TeleportPlayerBehaviour.java
+++ common/src/main/java/ru/maxthetomas/votvevents/behaviour/impl/TeleportPlayerBehaviour.java
@@ -3,7 +3,12 @@ package ru.maxthetomas.votvevents.behaviour.impl;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.portal.TeleportTransition;
+import net.minecraft.world.phys.Vec3;
 import ru.maxthetomas.votvevents.VotvEvents;
 import ru.maxthetomas.votvevents.behaviour.Behaviour;
 import ru.maxthetomas.votvevents.event.EventContext;
@@ -15,18 +20,21 @@ public class TeleportPlayerBehaviour extends Behaviour {
             Codec.FLOAT.fieldOf("x").forGetter(TeleportPlayerBehaviour::getX),
             Codec.FLOAT.fieldOf("y").forGetter(TeleportPlayerBehaviour::getY),
             Codec.FLOAT.fieldOf("z").forGetter(TeleportPlayerBehaviour::getZ),
+            Level.RESOURCE_KEY_CODEC.optionalFieldOf("level", null).forGetter(TeleportPlayerBehaviour::getLevel),
             Codec.BOOL.optionalFieldOf("relative", true).forGetter(TeleportPlayerBehaviour::isRelative)
     ).apply(instance, TeleportPlayerBehaviour::new));
 
-    private final float x;
-    private final float y;
-    private final float z;
+    private float x;
+    private float y;
+    private float z;
+    private final ResourceKey<Level> level;
     private final boolean relative;
 
-    public TeleportPlayerBehaviour(float x, float y, float z, boolean relative) {
+    public TeleportPlayerBehaviour(float x, float y, float z, ResourceKey<Level> level, boolean relative) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.level = level;
         this.relative = relative;
     }
 
@@ -48,6 +56,10 @@ public class TeleportPlayerBehaviour extends Behaviour {
         return z;
     }
 
+    public ResourceKey<Level> getLevel() {
+        return level;
+    }
+
     public boolean isRelative() {
         return relative;
     }
@@ -57,15 +69,30 @@ public class TeleportPlayerBehaviour extends Behaviour {
         super.execute(context);
         setDone(true);
 
-        if (context.getPlayer() == null) {
+        Player player = context.getPlayer();
+
+        if (player == null) {
             return;
         }
 
         if (relative) {
-            context.getPlayer().teleportRelative(x, y, z);
-        } else {
-            context.getPlayer().teleportTo(x, y, z);
+            x += (float) player.getX();
+            y += (float) player.getY();
+            z += (float) player.getZ();
         }
+
+        if (level != null) {
+            player.teleport(new TeleportTransition(
+                    context.getServer().getLevel(level),
+                    new Vec3(x, y, z),
+                    Vec3.ZERO,
+                    0,
+                    0,
+                    TeleportTransition.DO_NOTHING));
+            return;
+        }
+
+        player.teleportTo(x, y, z);
     }
 
     @Override
