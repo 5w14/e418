60bc9866e95bb9abb12baab8c5806c35f893857e
5w14 <max@maxthetomas.ru>
2025-07-21 19:28:35 +0300

    refactor: behaviour registration in one place

--- common/src/main/java/ru/maxthetomas/e418/behaviour/ActiveBehaviourDispatch.java
+++ common/src/main/java/ru/maxthetomas/e418/behaviour/ActiveBehaviourDispatch.java
@@ -4,47 +4,10 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.resources.ResourceLocation;
-import ru.maxthetomas.e418.behaviour.impl.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ActiveBehaviourDispatch<T extends Behaviour> {
-    private static final Map<ResourceLocation, MapCodec<? extends Behaviour>> STATE_CODECS = new HashMap<>();
-
     public static Codec<Behaviour> DISPATCH_CODEC = Codec.lazyInitialized(() ->
-            ResourceLocation.CODEC.dispatch(Behaviour::getTypeId, STATE_CODECS::get));
-
-    public static final MapCodec<? extends Behaviour> BROADCAST_CHAT_MESSAGE = register(BroadcastMessageBehaviour.ID, BroadcastMessageBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> DEBUG_PRINT_CONTEXT = register(DebugPrintContextBehaviour.ID, DebugPrintContextBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> EXECUTE_COMMAND = register(ExecuteCommandBehaviour.ID, ExecuteCommandBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> EXECUTE_EVENT = register(ExecuteEventBehaviour.ID, ExecuteEventBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> MODIFY_HELD_ITEM_COMPONENTS = register(ModifyHeldItemComponents.ID, ModifyHeldItemComponents.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> PLAY_SOUND = register(PlaySoundBehaviour.ID, PlaySoundBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> TELEPORT_PLAYER = register(TeleportPlayerBehaviour.ID, TeleportPlayerBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> SHOW_TOAST = register(ShowToastBehaviour.ID, ShowToastBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> SET_SHADER = register(SetShaderBehaviour.ID, SetShaderBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> GAME_CRASH = register(GameCrashBehaviour.ID, GameCrashBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> SNOW_OVERRIDE = register(SnowOverrideBehaviour.ID, SnowOverrideBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> DISABLE_NIGHT_SKIP = register(DisableNightSkipBehaviour.ID, DisableNightSkipBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> CANCEL_TIME_SKIP = register(CancelTimeSkipBehaviour.ID, CancelTimeSkipBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> BREAK_ATLAS = register(BreakAtlasBehaviour.ID, BreakAtlasBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> PREVENT_CHAT_USAGE = register(PreventChatUsageBehaviour.ID, PreventChatUsageBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> META_PARANOIA = register(MetaParanoiaBehaviour.ID, MetaParanoiaBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> SET_SUN_TEXTURE = register(SetSunTextureBehaviour.ID, SetSunTextureBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> SET_MOON_TEXTURE = register(SetMoonTextureBehaviour.ID, SetMoonTextureBehaviour.STATE_CODEC);
-
-    // Utilities
-    public static final MapCodec<? extends Behaviour> MUTATE_CONTEXT = register(MutateContextBehaviour.ID, MutateContextBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> TIMEOUT = register(TimeoutBehaviour.ID, TimeoutBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> WAIT_FOR_CONDITIONS = register(WaitForConditionBehaviour.ID, WaitForConditionBehaviour.STATE_CODEC);
-    public static final MapCodec<? extends Behaviour> STOP_EVENT = register(StopEventBehaviour.ID, StopEventBehaviour.STATE_CODEC);
-
-    private static MapCodec<? extends Behaviour> register(ResourceLocation type, MapCodec<? extends Behaviour> stateCodec) {
-        STATE_CODECS.put(type, stateCodec);
-        return stateCodec;
-    }
-
+            ResourceLocation.CODEC.dispatch(Behaviour::getTypeId, Behaviours.STATE_CODEC_REGISTRY::get));
 
     Behaviour.BehaviourState state;
     T activeBehaviour;
--- common/src/main/java/ru/maxthetomas/e418/behaviour/Behaviours.java
+++ common/src/main/java/ru/maxthetomas/e418/behaviour/Behaviours.java
@@ -14,31 +14,54 @@ import java.util.Map;
  */
 public class Behaviours {
     private static final Map<ResourceLocation, MapCodec<? extends Behaviour>> REGISTRY = new HashMap<>();
+    static final Map<ResourceLocation, MapCodec<? extends Behaviour>> STATE_CODEC_REGISTRY = new HashMap<>();
 
-    public static final MapCodec<? extends Behaviour> BROADCAST_CHAT_MESSAGE = register(BroadcastMessageBehaviour.ID, BroadcastMessageBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> DEBUG_PRINT_CONTEXT = register(DebugPrintContextBehaviour.ID, DebugPrintContextBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> EXECUTE_COMMAND = register(ExecuteCommandBehaviour.ID, ExecuteCommandBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> EXECUTE_EVENT = register(ExecuteEventBehaviour.ID, ExecuteEventBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> MODIFY_HELD_ITEM_COMPONENTS = register(ModifyHeldItemComponents.ID, ModifyHeldItemComponents.CODEC);
-    public static final MapCodec<? extends Behaviour> PLAY_SOUND = register(PlaySoundBehaviour.ID, PlaySoundBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> TELEPORT_PLAYER = register(TeleportPlayerBehaviour.ID, TeleportPlayerBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> SHOW_TOAST = register(ShowToastBehaviour.ID, ShowToastBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> SET_SHADER = register(SetShaderBehaviour.ID, SetShaderBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> GAME_CRASH = register(GameCrashBehaviour.ID, GameCrashBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> SNOW_OVERRIDE = register(SnowOverrideBehaviour.ID, SnowOverrideBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> DISABLE_NIGHT_SKIP = register(DisableNightSkipBehaviour.ID, DisableNightSkipBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> CANCEL_TIME_SKIP = register(CancelTimeSkipBehaviour.ID, CancelTimeSkipBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> BREAK_ATLAS = register(BreakAtlasBehaviour.ID, BreakAtlasBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> PREVENT_CHAT_USAGE = register(PreventChatUsageBehaviour.ID, PreventChatUsageBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> META_PARANOIA = register(MetaParanoiaBehaviour.ID, MetaParanoiaBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> SET_SUN_TEXTURE = register(SetSunTextureBehaviour.ID, SetSunTextureBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> SET_MOON_TEXTURE = register(SetMoonTextureBehaviour.ID, SetMoonTextureBehaviour.CODEC);
+    public static final MapCodec<? extends Behaviour> BROADCAST_CHAT_MESSAGE =
+            register(BroadcastMessageBehaviour.ID, BroadcastMessageBehaviour.CODEC, BroadcastMessageBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> DEBUG_PRINT_CONTEXT =
+            register(DebugPrintContextBehaviour.ID, DebugPrintContextBehaviour.CODEC, DebugPrintContextBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> EXECUTE_COMMAND =
+            register(ExecuteCommandBehaviour.ID, ExecuteCommandBehaviour.CODEC, ExecuteCommandBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> EXECUTE_EVENT =
+            register(ExecuteEventBehaviour.ID, ExecuteEventBehaviour.CODEC, ExecuteEventBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> MODIFY_HELD_ITEM_COMPONENTS =
+            register(ModifyHeldItemComponents.ID, ModifyHeldItemComponents.CODEC, ModifyHeldItemComponents.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> PLAY_SOUND =
+            register(PlaySoundBehaviour.ID, PlaySoundBehaviour.CODEC, PlaySoundBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> TELEPORT_PLAYER =
+            register(TeleportPlayerBehaviour.ID, TeleportPlayerBehaviour.CODEC, TeleportPlayerBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> SHOW_TOAST =
+            register(ShowToastBehaviour.ID, ShowToastBehaviour.CODEC, ShowToastBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> SET_SHADER =
+            register(SetShaderBehaviour.ID, SetShaderBehaviour.CODEC, SetShaderBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> GAME_CRASH =
+            register(GameCrashBehaviour.ID, GameCrashBehaviour.CODEC, GameCrashBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> SNOW_OVERRIDE =
+            register(SnowOverrideBehaviour.ID, SnowOverrideBehaviour.CODEC, SnowOverrideBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> DISABLE_NIGHT_SKIP =
+            register(DisableNightSkipBehaviour.ID, DisableNightSkipBehaviour.CODEC, DisableNightSkipBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> CANCEL_TIME_SKIP =
+            register(CancelTimeSkipBehaviour.ID, CancelTimeSkipBehaviour.CODEC, CancelTimeSkipBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> BREAK_ATLAS =
+            register(BreakAtlasBehaviour.ID, BreakAtlasBehaviour.CODEC, BreakAtlasBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> PREVENT_CHAT_USAGE =
+            register(PreventChatUsageBehaviour.ID, PreventChatUsageBehaviour.CODEC, PreventChatUsageBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> META_PARANOIA =
+            register(MetaParanoiaBehaviour.ID, MetaParanoiaBehaviour.CODEC, MetaParanoiaBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> SET_SUN_TEXTURE =
+            register(SetSunTextureBehaviour.ID, SetSunTextureBehaviour.CODEC, SetSunTextureBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> SET_MOON_TEXTURE =
+            register(SetMoonTextureBehaviour.ID, SetMoonTextureBehaviour.CODEC, SetMoonTextureBehaviour.STATE_CODEC);
 
     // Utilities
-    public static final MapCodec<? extends Behaviour> MUTATE_CONTEXT = register(MutateContextBehaviour.ID, MutateContextBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> TIMEOUT = register(TimeoutBehaviour.ID, TimeoutBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> WAIT_FOR_CONDITIONS = register(WaitForConditionBehaviour.ID, WaitForConditionBehaviour.CODEC);
-    public static final MapCodec<? extends Behaviour> STOP_EVENT = register(StopEventBehaviour.ID, StopEventBehaviour.CODEC);
+    public static final MapCodec<? extends Behaviour> MUTATE_CONTEXT =
+            register(MutateContextBehaviour.ID, MutateContextBehaviour.CODEC, MutateContextBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> TIMEOUT =
+            register(TimeoutBehaviour.ID, TimeoutBehaviour.CODEC, TimeoutBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> WAIT_FOR_CONDITIONS =
+            register(WaitForConditionBehaviour.ID, WaitForConditionBehaviour.CODEC, WaitForConditionBehaviour.STATE_CODEC);
+    public static final MapCodec<? extends Behaviour> STOP_EVENT =
+            register(StopEventBehaviour.ID, StopEventBehaviour.CODEC, StopEventBehaviour.STATE_CODEC);
 
     /**
      * Gets a codec to create a {@linkplain Behaviour} from the registry.
@@ -63,8 +86,9 @@ public class Behaviours {
      * @param codec The codec to register.
      * @return The second param, {@code codec}.
      */
-    public static MapCodec<? extends Behaviour> register(ResourceLocation id, MapCodec<? extends Behaviour> codec) {
+    public static MapCodec<? extends Behaviour> register(ResourceLocation id, MapCodec<? extends Behaviour> codec, MapCodec<? extends Behaviour> stateCodec) {
         REGISTRY.put(id, codec);
+        STATE_CODEC_REGISTRY.put(id, stateCodec);
         return codec;
     }
 }
