84960a900345e9a775b721d4753ec7e4b0ed4baa
5w14 <max@maxthetomas.ru>
2025-09-02 19:18:43 +0400

    feat: hide self-spoilers in commands (#18)

---
+++ common/src/main/java/ru/maxthetomas/e418/mixin/common/SharedSuggestionProviderMixin.java
@@ -0,0 +1,88 @@
+package ru.maxthetomas.e418.mixin.common;
+
+import com.google.common.base.Strings;
+import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ru.maxthetomas.e418.config.Config;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@Mixin(SharedSuggestionProvider.class)
+public interface SharedSuggestionProviderMixin {
+    @Inject(
+            method = "filterResources(Ljava/lang/Iterable;Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Consumer;)V",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static <T> void patchFilterResources(
+            Iterable<T> iterable,
+            String string,
+            Function<T, ResourceLocation> function,
+            Consumer<T> consumer,
+            CallbackInfo ci
+    ) {
+        if (!Config.isDebug()) return;
+
+        boolean bl = string.indexOf(':') > -1;
+
+        for (T object : iterable) {
+            ResourceLocation resourceLocation = function.apply(object);
+
+            // Skip if starts with "e418"
+            if (resourceLocation.toString().startsWith("e418")) {
+                continue;
+            }
+
+            if (bl) {
+                String string2 = resourceLocation.toString();
+                if (SharedSuggestionProvider.matchesSubStr(string, string2)) {
+                    consumer.accept(object);
+                }
+            } else if (SharedSuggestionProvider.matchesSubStr(string, resourceLocation.getNamespace())
+                    || (resourceLocation.getNamespace().equals("minecraft")
+                    && SharedSuggestionProvider.matchesSubStr(string, resourceLocation.getPath()))) {
+                consumer.accept(object);
+            }
+        }
+
+        ci.cancel();
+    }
+
+    @Inject(
+            method = "filterResources(Ljava/lang/Iterable;Ljava/lang/String;Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Consumer;)V",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static <T> void patchFilterResources2(
+            Iterable<T> iterable,
+            String string,
+            String string2,
+            Function<T, ResourceLocation> function,
+            Consumer<T> consumer,
+            CallbackInfo ci
+    ) {
+        if (!Config.isDebug()) return;
+
+        if (string.isEmpty()) {
+            iterable.forEach(object -> {
+                ResourceLocation rl = function.apply(object);
+                if (!rl.toString().startsWith("e418")) {
+                    consumer.accept(object);
+                }
+            });
+        } else {
+            String string3 = Strings.commonPrefix(string, string2);
+            if (!string3.isEmpty()) {
+                String string5 = string.substring(string3.length());
+                SharedSuggestionProvider.filterResources(iterable, string5, function, consumer);
+            }
+        }
+
+        ci.cancel();
+    }
+}
