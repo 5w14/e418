9e2322e2079ec7c862883e9aa0da724c13b4b97c
iller_saver <illersaver@gmail.com>
2025-09-19 01:02:27 +0300

    fix: forgot to remove part of a comment

--- common/src/main/java/ru/maxthetomas/e418/condition/impl/InDimensionCondition.java
+++ common/src/main/java/ru/maxthetomas/e418/condition/impl/InDimensionCondition.java
@@ -9,10 +9,6 @@ import ru.maxthetomas.e418.event.EventContext;
 
 /**
  * Returns true only when player is in specific dimension
- * <ul>
- *   <li><code>above</code> - Health above.</li>
- *   <li><code>below</code> - Health below.</li>
- * </ul>
  */
 public class InDimensionCondition implements ICondition {
     public static final ResourceLocation ID = E418.resLoc("in_dimension");
