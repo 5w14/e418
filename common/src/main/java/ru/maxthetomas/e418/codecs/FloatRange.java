f5e8266b0a47526a4126da4ddfb57bf9e3ea7c29
5w14 <max@maxthetomas.ru>
2025-09-19 04:25:05 +0400

    refactor: `FloatRange` codec validator

--- common/src/main/java/ru/maxthetomas/e418/codecs/FloatRange.java
+++ common/src/main/java/ru/maxthetomas/e418/codecs/FloatRange.java
@@ -1,16 +1,24 @@
 package ru.maxthetomas.e418.codecs;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 public record FloatRange(float min, float max) {
-    public static final MapCodec<FloatRange> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+    public static final MapCodec<FloatRange> CODEC = RecordCodecBuilder.<FloatRange>mapCodec(instance -> instance.group(
             Codec.FLOAT.fieldOf("min").forGetter(FloatRange::min),
             Codec.FLOAT.fieldOf("max").forGetter(FloatRange::max)
-    ).apply(instance, FloatRange::new));
+    ).apply(instance, FloatRange::new)).validate(FloatRange::validate);
 
     public boolean isIn(float value) {
         return value < max && value > min;
+    }
+
+    public static DataResult<FloatRange> validate(FloatRange floatRange) {
+        if (floatRange.min >= floatRange.max)
+            return DataResult.error(() -> "Invalid range provided, min is bigger than max.");
+
+        return DataResult.success(floatRange);
     }
 }
