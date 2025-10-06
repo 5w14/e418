a803dcb3f0b0a3a7045af4ea9576cb9df46dca96
5w14 <max@maxthetomas.ru>
2025-10-07 03:07:27 +0400

    refactor: translatable

--- common/src/main/java/ru/maxthetomas/e418/gui/onboarding/OnboardingScreen.java
+++ common/src/main/java/ru/maxthetomas/e418/gui/onboarding/OnboardingScreen.java
@@ -19,31 +19,31 @@ public class OnboardingScreen extends Screen {
     @Override
     protected void init() {
         addRenderableWidget(
-                WidgetWrapper.create(new StringWidget(Component.literal("e418 configuration"), font))
+                WidgetWrapper.create(new StringWidget(Component.translatable("e418.screen.onboarding.title"), font))
                         .y(20).centerX(this.width).apply(StringWidget::alignLeft).widget()
         );
 
         var explanation = addRenderableWidget(
                 WidgetWrapper.create(new MultiLineTextWidget(
-                                Component.literal("Long text ".repeat(70)), font))
+                                Component.translatable("e418.screen.onboarding.description"), font))
                         .apply(s -> s.setMaxWidth(300))
                         .y(50).w(300).centerX(this.width).widget()
         );
 
         addRenderableWidget(
-                WidgetWrapper.create(new Button.Builder(Component.literal("Button"), x -> {
+                WidgetWrapper.create(new Button.Builder(Component.translatable("e418.screen.onboarding.continue"), x -> {
                         }).build())
                         .centerX(this.width).y(explanation.getHeight() + 65).widget()
         );
 
         addRenderableWidget(
-                WidgetWrapper.create(new Button.Builder(Component.literal("Skip"),
+                WidgetWrapper.create(new Button.Builder(Component.translatable("e418.screen.onboarding.skip"),
                                 x -> this.minecraft.setScreen(parent)).build())
                         .bottom(this.height, 20).x(this.width / 2 - 104).w(100).widget()
         );
 
         addRenderableWidget(
-                WidgetWrapper.create(new Button.Builder(Component.literal("Continue"), x -> {
+                WidgetWrapper.create(new Button.Builder(Component.translatable("e418.screen.onboarding.continue"), x -> {
                         }).build())
                         .bottom(this.height, 20).x(this.width / 2 + 4).w(100).widget()
         );
